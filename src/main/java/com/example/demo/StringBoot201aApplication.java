63d72f3b22e73bc48be52d33f8b772a89b57c525
prasantib <prasanti.babu@gmail.com>
2019-10-07 17:07:07 -0400

    string boot 201a evercise

---
+++ src/main/java/com/example/demo/StringBoot201aApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringBoot201aApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringBoot201aApplication.class, args);
+    }
+
+}
